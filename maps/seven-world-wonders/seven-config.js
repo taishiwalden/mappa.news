7ff1b9c4de0f4e1c2f2f960f88c5cc585a2ea0c1
Taishi <taishw@uw.edu>
2022-02-27 13:58:50 -0800

    Added config

--- maps/seven-world-wonders/seven-config.js
+++ maps/seven-world-wonders/seven-config.js
@@ -11,86 +11,90 @@ var config = {
     theme: "light",
     alignment: "left",
     chapters: [
-        //Afro-Eurasia Overview
+        //Overview
         {
             id: "48d352fe5c88b29680f9ce8d7a233191",
+            title: "Introduction",
+            description:
+                "Our world is filled with the most unique structures that are both man-made and natural. Some of the man-made creations include churches, tombs, temples, monuments, mosques, buildings and even cities. These structures have withstood the test of time and they continue to leave many awestruck with their brilliance. There are many in the world, but here are the 7 World Wonders as voted by 10 million people.",
+            location: {
+                center: [-35.016, 3.026],
+                zoom: 1.87,
+                pitch: 0,
+                bearing: 0,
+            },
+        },
+
+        //1. Taj Mahal           The start of           "THE 7 WORLD WONDERS"
+        {
+            id: "5c76ad75992aaac989465955479e2fb4",
             title: "1. The Taj Mahal",
             description:
                 "The Taj Mahal is known the world over for its beautiful architecture, history and the tale of love that led to its creation. ",
             location: {
-                center: [78.042191, 27.173169],
-                zoom: 2.26,
+                center: [74.1, 26.286],
+                zoom: 5.18,
                 pitch: 0,
                 bearing: 0,
             },
         },
 
-        //THE NEW SILK ROAD: Zoom into Beijing, China
+        //2. The Colosseum
         {
-            id: "5c76ad75992aaac989465955479e2fb4",
+            id: "7ba8dfb2d8196a463676254236476581",
             title: "2. The Colosseum",
             description:
                 "Also known as the Flavian Amphitheatre, the Colosseum is an oval-shaped amphitheater located in the centre of Rome in Italy. It is the largest amphitheater in the world, and had the capacity to host about 80,000 spectators. In ancient times, it was used for gladiatorial contests and public spectacles such as mock sea battles.",
             location: {
-                center: [115.443, 40.054],
-                zoom: 6.86,
+                center: [41.891, 12.47],
+                zoom: 12.49,
                 pitch: 0,
                 bearing: 0,
             },
         },
-
-        //THE NEW SILK ROAD: Xian, China            The start of           "THE NEW SILK ROAD"
-        {
-            id: "7ba8dfb2d8196a463676254236476581",
-            title: "Xian, China",
-            description: "Add text here about Xian",
-            location: {
-                center: [103.706, 36.704],
-                zoom: 4.98,
-                pitch: 0,
-                bearing: 0,
-            },
-        },
-        //THE NEW SILK ROAD: Almaty, Kazakhstan
+        //3. Chichen Itza
         {
             id: "9e34f44c4e4aa2afecbf0e85c16e72af",
-            title: "Almaty, Kazakhstan",
-            description: "Add text here",
+            title: "3. Chichen Itza",
+            description:
+                "Chichen Itza is an archaeological site located in Tinum Municipality of Yucatan State in Mexico. It was a large pre-Columbian city built by the Maya people, and was likely to have been one of the mythical great cities. It is one of the most visited archaeological sites in Mexico, and consists of buildings like the El Caracol or observatory of Chichen Itza, Temple of the Warriors, Grand Ballcourt, Venus Platform, and others.",
             location: {
-                center: [72.558, 45.62],
-                zoom: 5.19,
+                center: [20.288, -90.66],
+                zoom: 5.48,
                 pitch: 0,
                 bearing: 0,
             },
         },
-        //THE NEW SILK ROAD: Samarkand, Uzekistan
+        //4. Machu Picchu
         {
             id: "qatbspsiyczc7r1pu4zqkrgru08du0g7",
-            title: "Samarkand, Uzekistan",
-            description: "Add text here",
+            title: "4. Machu Picchu",
+            description:
+                "Located in the Eastern Cordillera of southern Peru on a 7,970 ft mountain ridge, Machu Picchu is a 15th-century Inca citadel. It is the most familiar icon of Inca civilization, and is often mistaken for the ‘Lost City of the Incas’.",
             location: {
-                center: [64.907, 40.488],
-                zoom: 5.78,
+                center: [-13.203, -74.881],
+                zoom: 5.31,
                 pitch: 0,
                 bearing: 0,
             },
         },
-        //THE NEW SILK ROAD: Dushanbe, Tajikistan
+        //5. Christ the Redeemer
         {
             id: "ra7514ik8muffrpz80l1rl839291e1pi",
-            title: "Dushanbe, Tajikistan",
-            description: "Add text here",
+            title: "5. Christ the Redeemer",
+            description:
+                "A very well known work of art in Brazil, Christ the Redeemer is an Art Deco statue of Jesus Christ. Made of reinforced concrete and soapstone, it weighs 635 metric tons and is located at the peak of the 700-metre Corcovado mountain in the Tijuca Forest National Park overlooking the city of Rio de Janeiro. Construction began in 1922 and it was completed in 1931.",
             location: {
-                center: [69.935, 38.483],
-                zoom: 6.59,
+                center: [39.988, 15.712],
+                zoom: 13.29,
                 pitch: 0,
                 bearing: 0,
             },
         },
-        //THE NEW SILK ROAD: Tehran, Iran
+        //6. Petra, Jordan
         {
             id: "bo1lysqbm6r8wu60e2e86nvmt1n5q8cl",
-            title: "Tehran, Iran",
+            title: "6. Petra, Jordan",
             description: "Add text here",
             location: {
                 center: [51.216, 36.101],
@@ -99,10 +103,10 @@ var config = {
                 bearing: 0,
             },
         },
-        //THE NEW SILK ROAD: Istanbul, Turkey
+        //7. the Great Wall of China
         {
             id: "qkvjpfpvj7d2e9uzvbxla01c5vp1k1g5",
-            title: "Istanbul, Turkey",
+            title: "7. the Great Wall of China",
             description: "Add text here",
             location: {
                 center: [29.572, 41.141],
@@ -111,38 +115,26 @@ var config = {
                 bearing: 0,
             },
         },
-        //THE NEW SILK ROAD: Moscow, Russia
+        //Summary
         {
             id: "t8prwxafovqw4ieem0w1ymfmsqxazpav",
-            title: "Moscow, Russia",
+            title: "Summary",
             description: "Add text here",
             location: {
-                center: [34.487, 55.418],
-                zoom: 4.17,
+                center: [-35.016, 3.026],
+                zoom: 1.87,
                 pitch: 0,
                 bearing: 0,
             },
         },
-        //THE NEW SILK ROAD: Dusseldorf, Germany
+        //Credit
         {
             id: "nq6v66f45ow16xzfgo46zdo5o3a7lgo6",
-            title: "Dusseldorf, Germany",
+            title: "Credit",
             description: "Add text here",
             location: {
                 center: [3.056, 51.658],
                 zoom: 5.02,
-                pitch: 0,
-                bearing: 0,
-            },
-        },
-        //THE NEW SILK ROAD: Venice, Italy            THE END OF            "THE NEW SILK ROAD"
-        {
-            id: "c2hd1f3zxj4jyq1lolqvptdblutdmuoh",
-            title: "Venice, Italy",
-            description: "Add text here",
-            location: {
-                center: [11.37, 45.634],
-                zoom: 7.39,
                 pitch: 0,
                 bearing: 0,
             },
