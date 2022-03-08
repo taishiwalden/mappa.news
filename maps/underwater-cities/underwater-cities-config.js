bbafd3a0d9bd58cd4271e1e7c1b72d719fd7a79e
Taishi <taishw@uw.edu>
2022-03-08 15:34:14 -0800

    changed unique ids

--- maps/underwater-cities/underwater-cities-config.js
+++ maps/underwater-cities/underwater-cities-config.js
@@ -13,7 +13,7 @@ var config = {
     chapters: [
         //Overview
         {
-            id: "48d352fe5c88b29680f9ce8d7a233191",
+            id: "48d352fe5c88b29680f9ce8d7a233191", //ID's should be unique
             title: "Overview",
             description:
                 "There is little doubt our oceans are rising and cities will go under. It is no longer an issue of if but when. No one can predict exactly when, however, based on simple elevation maps, we can predict which major world cities are most likely to be underwater first. The maps we show depict the impact once sea levels rise 1.5 meters (5 ft) which is likely should global temperatures increase by 3°C (5.4°F).",
@@ -119,7 +119,7 @@ var config = {
         },
         //8. Jakarta, Indonesia
         {
-            id: "qkvjpfpvj7d2e9uzvbxla01c5vp1k1g5",
+            id: "b8wosQpXwGgGqgLXfPiSNWMWMHjczTGx",
             title: "8. Jakarta, Indonesia",
             description:
                 "Jakarta, Indonesia's massive capital, sits on the northwest coast of the island of Java. A historic mix of cultures – Javanese, Malay, Chinese, Arab, Indian and European – has influenced its architecture, language and cuisine. POPULATION SIZE: 10.5 million || ELEVATION: 26 ft",
@@ -132,7 +132,7 @@ var config = {
         },
         //9. London, England
         {
-            id: "qkvjpfpvj7d2e9uzvbxla01c5vp1k1g5",
+            id: "hiF5Bl7YjwetWFZSRL2wchr48o55ubny",
             title: "9. London, England",
             description:
                 "London, the capital of England and the United Kingdom, is a 21st-century city with history stretching back to Roman times. At its centre stand the imposing Houses of Parliament, the iconic ‘Big Ben’ clock tower and Westminster Abbey, site of British monarch coronations. POPULATION SIZE: 9.5 million || ELEVATION: 36 ft",
@@ -145,7 +145,7 @@ var config = {
         },
         //10. Dhaka, Bangladesh
         {
-            id: "qkvjpfpvj7d2e9uzvbxla01c5vp1k1g5",
+            id: "2Rle7YPkdEypnsQf13iYXn2kPsSGz3wf",
             title: "10. Dhaka, Bangladesh",
             description:
                 "Dhaka is the capital city of Bangladesh, in southern Asia. Set beside the Buriganga River, it’s at the center of national government, trade and culture. POPULATION SIZE: 8.9 million || ELEVATION: 13 ft",
@@ -158,7 +158,7 @@ var config = {
         },
         //11. Ho Chi Min City, Vietnam
         {
-            id: "qkvjpfpvj7d2e9uzvbxla01c5vp1k1g5",
+            id: "VLlxVffj2zfwvWQI9nY8LxJq7ffHaIm9",
             title: "11. Ho Chi Min City, Vietnam",
             description:
                 "Ho Chi Minh City, formerly known as Saigon, is the largest city in Vietnam, situated in the south. In the southeastern region, the city surrounds the Saigon River and covers about 2,061 km².. POPULATION SIZE: 8.8 million || ELEVATION: 63 ft",
@@ -171,7 +171,7 @@ var config = {
         },
         //12. San Francisco, USA
         {
-            id: "qkvjpfpvj7d2e9uzvbxla01c5vp1k1g5",
+            id: "ghKq6ksx5R7qPcmcpffA5AMPAVIOOKMB",
             title: "12. San Francisco, USA",
             description:
                 "San Francisco, officially the City and County of San Francisco, is a cultural, commercial, and financial center in the U.S. state of California.. POPULATION SIZE: 7.8 million || ELEVATION: 52 ft",
