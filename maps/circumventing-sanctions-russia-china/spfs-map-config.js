8a8fd1c09ec6dbff36a1778c64424a659018803a
Taishi <taishw@uw.edu>
2022-03-22 14:41:28 -0700

    added russian maps

---
+++ maps/circumventing-sanctions-russia-china/spfs-map-config.js
@@ -0,0 +1,151 @@
+// this is for the CIRCUMVENTING SANCTIONS scrollytelling map
+// DAVE MOLESKE SPFS
+
+var config = {
+    style: "mapbox://styles/taishiwalden/cl0h4z6dg004u15m6i0j5jkh6", //TODO
+    accessToken: "pk.eyJ1IjoidGFpc2hpd2FsZGVuIiwiYSI6ImNrcXkzaWJvbzE0Zzgyd21mZHVjNDBvYmIifQ.E2__hXdmUMAPh2zjvGjJgw", //TODO
+    title: "System for Transfer of Financial Messages (SPFS)", //TODO
+    subtitle: "Circumventing Sanctions: Russia and China", //TODO
+    byline: "By Dave Moleske", //TODO
+    footer: "©2022 Dave Moleske",
+    showMarkers: false,
+    theme: "light",
+    alignment: "left",
+    chapters: [
+        //SPFS Overview
+        {
+            id: "48d352fe5c88b29680f9ce8d7a233191",
+            title: "What is SPFS?",
+            description: "SPFS description",
+            location: {
+                center: [-21.82013, 12.85017],
+                zoom: 1.32,
+                pitch: 0,
+                bearing: 0,
+            },
+        },
+
+        //CIRCUMVENTING SANCTIONS: SPFS
+        {
+            id: "5c76ad75992aaac989465955479e2fb4",
+            title: "Beijing",
+            description:
+                "China’s President Xi Jinping unveiled The Initiative, officially stating that the BRI aims to 'promote the connectivity of Asian, European and African continents and their adjacent seas, establish and strengthen partnerships among the countries along the Belt and Road, set up all-dimensional, multi-tiered and composite connectivity networks, and realize diversified, independent, balanced and sustainable development in these countries.'",
+            location: {
+                center: [115.443, 40.054],
+                zoom: 6.86,
+                pitch: 0,
+                bearing: 0,
+            },
+        },
+
+        //CIRCUMVENTING SANCTIONS:
+        {
+            id: "7ba8dfb2d8196a463676254236476581",
+            title: "Xian, China",
+            description: "Add text here about Xian",
+            location: {
+                center: [103.706, 36.704],
+                zoom: 4.98,
+                pitch: 0,
+                bearing: 0,
+            },
+        },
+        //CIRCUMVENTING SANCTIONS:
+        {
+            id: "9e34f44c4e4aa2afecbf0e85c16e72af",
+            title: "Almaty, Kazakhstan",
+            description: "Add text here",
+            location: {
+                center: [72.558, 45.62],
+                zoom: 5.19,
+                pitch: 0,
+                bearing: 0,
+            },
+        },
+        //CIRCUMVENTING SANCTIONS:
+        {
+            id: "qatbspsiyczc7r1pu4zqkrgru08du0g7",
+            title: "Samarkand, Uzekistan",
+            description: "Add text here",
+            location: {
+                center: [64.907, 40.488],
+                zoom: 5.78,
+                pitch: 0,
+                bearing: 0,
+            },
+        },
+        //CIRCUMVENTING SANCTIONS:
+        {
+            id: "ra7514ik8muffrpz80l1rl839291e1pi",
+            title: "Dushanbe, Tajikistan",
+            description: "Add text here",
+            location: {
+                center: [69.935, 38.483],
+                zoom: 6.59,
+                pitch: 0,
+                bearing: 0,
+            },
+        },
+        //CIRCUMVENTING SANCTIONS:
+        {
+            id: "bo1lysqbm6r8wu60e2e86nvmt1n5q8cl",
+            title: "Tehran, Iran",
+            description: "Add text here",
+            location: {
+                center: [51.216, 36.101],
+                zoom: 7,
+                pitch: 0,
+                bearing: 0,
+            },
+        },
+        //CIRCUMVENTING SANCTIONS:
+        {
+            id: "qkvjpfpvj7d2e9uzvbxla01c5vp1k1g5",
+            title: "Istanbul, Turkey",
+            description: "Add text here",
+            location: {
+                center: [29.572, 41.141],
+                zoom: 7.19,
+                pitch: 0,
+                bearing: 0,
+            },
+        },
+        //CIRCUMVENTING SANCTIONS:
+        {
+            id: "t8prwxafovqw4ieem0w1ymfmsqxazpav",
+            title: "Moscow, Russia",
+            description: "Add text here",
+            location: {
+                center: [34.487, 55.418],
+                zoom: 4.17,
+                pitch: 0,
+                bearing: 0,
+            },
+        },
+        //CIRCUMVENTING SANCTIONS:
+        {
+            id: "nq6v66f45ow16xzfgo46zdo5o3a7lgo6",
+            title: "Dusseldorf, Germany",
+            description: "Add text here",
+            location: {
+                center: [3.056, 51.658],
+                zoom: 5.02,
+                pitch: 0,
+                bearing: 0,
+            },
+        },
+        //CIRCUMVENTING SANCTIONS:             THE END OF            "CIRCUMVENTING SANCTIONS"
+        {
+            id: "c2hd1f3zxj4jyq1lolqvptdblutdmuoh",
+            title: "Venice, Italy",
+            description: "Add text here",
+            location: {
+                center: [11.37, 45.634],
+                zoom: 7.39,
+                pitch: 0,
+                bearing: 0,
+            },
+        },
+    ],
+};
