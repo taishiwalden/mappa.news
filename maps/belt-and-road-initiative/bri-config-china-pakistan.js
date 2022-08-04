1fbab1e444bb7068e9064bc91d439e75067ecaf7
Taishi <taishw@uw.edu>
2022-08-04 11:23:50 -0700

    changed the locations in the pakistan corridor

--- maps/belt-and-road-initiative/bri-config-china-pakistan.js
+++ maps/belt-and-road-initiative/bri-config-china-pakistan.js
@@ -2,151 +2,54 @@
 // The China Pakistan Corridor
 
 var config = {
-    style: "mapbox://styles/taishiwalden/ckzovzbqj000714qnd5m3cdcm", //TODO
-    accessToken: "pk.eyJ1IjoidGFpc2hpd2FsZGVuIiwiYSI6ImNrcXkzaWJvbzE0Zzgyd21mZHVjNDBvYmIifQ.E2__hXdmUMAPh2zjvGjJgw", //TODO
-    title: "The Belt and Road Initiative", //TODO
-    subtitle: "China–Pakistan Economic Corridor (CPEC)", //TODO
-    byline: "By Taishi Walden", //TODO
-    footer: "©2022 Taishi Walden",
-    showMarkers: false,
-    theme: "light",
-    alignment: "left",
-    chapters: [
-        //Afro-Eurasia Overview
-        {
-            id: "48d352fe5c88b29680f9ce8d7a233191",
-            title: "What is the China Pakistan Corridor?",
-            description:
-                "Beginning in 2013, CPEC is a collection of infrastructure projects that are under construction through out the China-Pakistan region.  The value of CPEC projects is worth $62 billion as of 2020.",
-            location: {
-                center: [55.38174, 33.5322],
-                zoom: 4,
-                pitch: 38,
-                bearing: 0,
-            },
-        },
+  style: "mapbox://styles/taishiwalden/ckzovzbqj000714qnd5m3cdcm", //TODO
+  accessToken: "pk.eyJ1IjoidGFpc2hpd2FsZGVuIiwiYSI6ImNrcXkzaWJvbzE0Zzgyd21mZHVjNDBvYmIifQ.E2__hXdmUMAPh2zjvGjJgw", //TODO
+  title: "The Belt and Road Initiative", //TODO
+  subtitle: "China–Pakistan Economic Corridor (CPEC)", //TODO
+  byline: "By Taishi Walden", //TODO
+  footer: "©2022 Taishi Walden",
+  showMarkers: false,
+  theme: "light",
+  alignment: "left",
+  chapters: [
+    //Afro-Eurasia Overview
+    {
+      id: "48d352fe5c88b29680f9ce8d7a233191",
+      title: "What is the China Pakistan Corridor?",
+      description:
+        "Beginning in 2013, CPEC is a collection of infrastructure projects that are under construction through out the China-Pakistan region.  The value of CPEC projects is worth $62 billion as of 2020.",
+      location: {
+        center: [55.38174, 33.5322],
+        zoom: 4,
+        pitch: 38,
+        bearing: 0,
+      },
+    },
 
-        //CHINA PAKISTAN CORRIDOR: begin
-        {
-            id: "5c76ad75992aaac989465955479e2fb4",
-            title: "Its Purpose",
-            description:
-                "The mission of this project is to upgrade Pakistan's required infrastructure and strengthen its economy. By expanding motorways, facilitating mass transit, and establishing special economic zones and energy production areas.",
-            location: {
-                center: [67.47066, 40.05495],
-                zoom: 0,
-                pitch: 0,
-                bearing: 0,
-            },
-        },
-
-        //CHINA PAKISTAN CORRIDOR: Middle
-        {
-            id: "7ba8dfb2d8196a463676254236476581",
-            title: "How much of it is completed?",
-            description: "According to 'cpec.gov.pk/progress-update', it is currently about 73% complete.",
-            location: {
-                center: [103.706, 36.704],
-                zoom: 0,
-                pitch: 0,
-                bearing: 0,
-            },
-        },
-        //CHINA PAKISTAN CORRIDOR: Middle
-        {
-            id: "9e34f44c4e4aa2afecbf0e85c16e72af",
-            title: "Almaty, Kazakhstan",
-            description: "Add text here about CHINA PAKISTAN CORRIDOR",
-            location: {
-                center: [72.558, 45.62],
-                zoom: 0,
-                pitch: 0,
-                bearing: 0,
-            },
-        },
-        //CHINA PAKISTAN CORRIDOR: Middle
-        {
-            id: "qatbspsiyczc7r1pu4zqkrgru08du0g7",
-            title: "Samarkand, Uzekistan",
-            description: "Add text here",
-            location: {
-                center: [64.907, 40.488],
-                zoom: 5.78,
-                pitch: 0,
-                bearing: 0,
-            },
-        },
-        //THE NEW SILK ROAD: Dushanbe, Tajikistan
-        {
-            id: "ra7514ik8muffrpz80l1rl839291e1pi",
-            title: "Dushanbe, Tajikistan",
-            description: "Add text here",
-            location: {
-                center: [69.935, 38.483],
-                zoom: 6.59,
-                pitch: 0,
-                bearing: 0,
-            },
-        },
-        //THE NEW SILK ROAD: Tehran, Iran
-        {
-            id: "bo1lysqbm6r8wu60e2e86nvmt1n5q8cl",
-            title: "Tehran, Iran",
-            description: "Add text here",
-            location: {
-                center: [51.216, 36.101],
-                zoom: 7,
-                pitch: 0,
-                bearing: 0,
-            },
-        },
-        //THE NEW SILK ROAD: Istanbul, Turkey
-        {
-            id: "qkvjpfpvj7d2e9uzvbxla01c5vp1k1g5",
-            title: "Istanbul, Turkey",
-            description: "Add text here",
-            location: {
-                center: [29.572, 41.141],
-                zoom: 7.19,
-                pitch: 0,
-                bearing: 0,
-            },
-        },
-        //THE NEW SILK ROAD: Moscow, Russia
-        {
-            id: "t8prwxafovqw4ieem0w1ymfmsqxazpav",
-            title: "Moscow, Russia",
-            description: "Add text here",
-            location: {
-                center: [34.487, 55.418],
-                zoom: 4.17,
-                pitch: 0,
-                bearing: 0,
-            },
-        },
-        //THE NEW SILK ROAD: Dusseldorf, Germany
-        {
-            id: "nq6v66f45ow16xzfgo46zdo5o3a7lgo6",
-            title: "Dusseldorf, Germany",
-            description: "Add text here",
-            location: {
-                center: [3.056, 51.658],
-                zoom: 5.02,
-                pitch: 0,
-                bearing: 0,
-            },
-        },
-        //THE NEW SILK ROAD: Venice, Italy            THE END OF            "THE NEW SILK ROAD"
-        {
-            id: "c2hd1f3zxj4jyq1lolqvptdblutdmuoh",
-            title: "Venice, Italy",
-            description: "Add text here",
-            location: {
-                center: [11.37, 45.634],
-                zoom: 7.39,
-                pitch: 0,
-                bearing: 0,
-            },
-        },
-    ],
+    //CHINA PAKISTAN CORRIDOR: begin
+    {
+      id: "5c76ad75992aaac989465955479e2fb4",
+      title: "Its Purpose",
+      description:
+        "The mission of this project is to upgrade Pakistan's required infrastructure and strengthen its economy. By expanding motorways, facilitating mass transit, and establishing special economic zones and energy production areas.",
+      location: {
+        center: [74.79029, 36.39985],
+        zoom: 6,
+        pitch: 43,
+        bearing: 0,
+      },
+    },
+    //THE NEW SILK ROAD: Islamabad, Pakistan
+    {
+      id: "t8prwxafovqw4ieem0w1ymfmsqxazpav",
+      title: "Islamabad, Pakistan",
+      description: "Add text here",
+      location: {
+        center: [68.10577, 27.22365],
+        zoom: 6.3,
+        pitch: 60,
+        bearing: -12,
+      },
+    },
+  ],
 };
